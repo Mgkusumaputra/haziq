d4a092d336716fb5932adaa57ed44e7a8a0b2161
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-07-02 23:12:27 +0700

    💬 Texts: change copywriting for Hero Section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <div className="mt-32 w-full flex flex-col px-6 max-w-5xl mx-auto">
             <div className="flex items-center justify-center mb-16">
-                <h1 className="font-bold text-3xl">Hello World</h1>
+                <h1 className="font-bold text-3xl">Baca Al-Quran dan Tafsir Ayat Dengan Mudah Bersama Haziq</h1>
             </div>
             <div className="flex flex-col gap-3 md:grid md:grid-cols-3">
                 <CardHero href="/doa" title="Kumpulan Doa" />
