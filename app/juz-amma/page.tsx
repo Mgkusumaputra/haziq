ed04e9e0be9d20e3bcc4e202f37431b88e02e60f
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-07-02 10:13:30 +0700

    ✨ Feat: juz amma page

---
+++ app/juz-amma/page.tsx
@@ -0,0 +1,33 @@
+import CardSurah from "../../components/home/CardSurah";
+
+export default async function JuzAmma() {
+    const res = await fetch("https://equran.id/api/v2/surat");
+    const getAllSurah = await res.json();
+    const filterJuzAmma = getAllSurah.data.slice(77, 114);
+
+    return (
+        <div className="w-full flex flex-col items-center px-6 max-w-5xl gap-3 mt-28 mb-7 mx-auto">
+            <h1 className="text-2xl font-semibold underline">Juz Amma</h1>
+            <input
+                type="search"
+                placeholder="🔍 Cari Surah"
+                className="border-2 border-sky-500 bg-slate-50 dark:bg-transparent w-full px-3 py-2 mt-5 rounded-md"
+            />
+            <div className="flex flex-col gap-3 w-full md:grid md:grid-cols-3">
+                {filterJuzAmma.map((data: any) => {
+                    return (
+                        <CardSurah
+                            key={data.nomor}
+                            href={`/surah/${data.nomor}`}
+                            noSurah={data.nomor}
+                            namaSurah={data.namaLatin}
+                            artiSurah={data.arti}
+                            tempatTurun={data.tempatTurun}
+                            jumlahAyat={data.jumlahAyat}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,6 @@
 import CardHero from "@components/home/CardHero";
 
 export default function Hero() {
-
     return (
         <div className="mt-32 w-full flex flex-col px-6 max-w-5xl mx-auto">
             <div className="flex items-center justify-center mb-16">
@@ -9,7 +8,7 @@ export default function Hero() {
             </div>
             <div className="flex flex-col gap-3 md:grid md:grid-cols-3">
                 <CardHero href="/" title="Kumpulan Doa" />
-                <CardHero href="/" title="Juz Amma" />
+                <CardHero href="/juz-amma" title="Juz Amma" />
                 <CardHero href="/" title="Ayat Favorit" />
             </div>
         </div>
