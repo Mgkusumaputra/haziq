aed94916825a190bd27b573dbaa5713cf816f6fb
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-07-03 18:23:57 +0700

    💄 UI: changing Navbar & Footer Font Size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <>
-            <div className="w-full py-8 flex flex-col md:flex-row items-center justify-center gap-1 bg-slate-50 text-gray-950 dark:bg-slate-700 dark:text-gray-50">
-                <p className="text-lg">Copyright © {new Date().getFullYear()} Haziq - Made with ❤ by</p>
+            <div className="w-full py-8 flex flex-col md:flex-row items-center justify-center gap-1 text-sm md:text-lg bg-slate-50 text-gray-950 dark:bg-slate-700 dark:text-gray-50">
+                <p className="">Copyright © {new Date().getFullYear()} Haziq - Made with ❤ by</p>
                 <Link target="_blank" href="https://mgkusumaputra.me" className="underline font-bold">
                     Mgkusumaputra
                 </Link>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
+import Link from "next/link";
 
 import { useTheme } from "next-themes";
 
 import Sun from "@icons/Sun";
 import Moon from "@icons/Moon";
-import Link from "next/link";
 
 export function Navbar() {
     const { theme, setTheme } = useTheme();
@@ -14,7 +14,9 @@ export function Navbar() {
             <nav className="fixed top-0 left-0 right-0 z-20 flex items-center justify-center w-full py-4 px-6 bg-slate-50 dark:bg-slate-700">
                 <div className="flex items-center justify-between max-w-5xl px-6 w-full">
                     <Link href="/">
-                        <strong className="font-bold text-2xl">Haziq</strong>
+                        <h2 className="flex items-center gap-2 font-bold text-2xl">
+                            Haziq
+                        </h2>
                     </Link>
                     <button
                         className="p-1 border-2 border-slate-200 rounded-md drop-shadow-lg dark:border-slate-500"
