2d33062c8bd328ceb145969b176d68f544e84ce8
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-07-04 08:42:56 +0700

    🔍 SEO: fixing OG to metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,33 +18,28 @@ export const metadata = {
     title: "Haziq - Al-Quran Online Bahasa Indonesia",
     description:
         "Nikmati pengalaman Membaca Al-Quran yang praktis dan interaktif dengan Haziq. Baca Al-Quran dan tafsir ayat dalam bahasa Indonesia dengan mudah",
+    keywords: ["Haziq", "Al-Quran Online", "Baca Al-Quran", "Tafsir Ayat Digital", "Al-Quran Bahasa Indonesia"],
+    openGraph: {
+        title: "Haziq - Al-Quran Online Bahasa Indonesia",
+        description:
+            "Nikmati pengalaman Membaca Al-Quran yang praktis dan interaktif dengan Haziq. Baca Al-Quran dan tafsir ayat dalam bahasa Indonesia dengan mudah",
+        url: "https://haziq.mgkusumaputra.me",
+        siteName: "Next.js",
+        images: ["/images/OG_Banner.png"],
+        type: "website",
+    },
+    twitter: {
+        card: "/images/OG_Banner.png",
+        title: "Haziq - Al-Quran Online Bahasa Indonesia",
+        description:
+            "Nikmati pengalaman Membaca Al-Quran yang praktis dan interaktif dengan Haziq. Baca Al-Quran dan tafsir ayat dalam bahasa Indonesia dengan mudah",
+        images: ["/images/OG_Banner.png"],
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <Head>
-                {/* <!-- Open Graph / Facebook --> */}
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://haziq.mgkusumaputra.me" />
-                <meta property="og:title" content="Haziq — Al-Quran Online Bahasa Indonesia" />
-                <meta
-                    property="og:description"
-                    content="Nikmati pengalaman Membaca Al-Quran yang praktis dan interaktif dengan Haziq. Baca Al-Quran dan tafsir ayat dalam bahasa Indonesia dengan mudah"
-                />
-                <meta property="og:image" content="/images/OG_Banner.png" />
-
-                {/* <!-- Twitter --> */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://haziq.mgkusumaputra.me" />
-                <meta property="twitter:title" content="Haziq — Al-Quran Online Bahasa Indonesia" />
-                <meta
-                    property="twitter:description"
-                    content="Nikmati pengalaman Membaca Al-Quran yang praktis dan interaktif dengan Haziq. Baca Al-Quran dan tafsir ayat dalam bahasa Indonesia dengan mudah"
-                />
-                <meta property="twitter:image" content="/images/OG_Banner.png" />
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-            </Head>
             <ThemeProvider>
                 <body
                     className={`${quicksand.className} bg-slate-300 text-gray-950 dark:bg-slate-800 dark:text-gray-50`}
